2eb43a4f748e22224c11ec8879a5375feed2b7fd
Abigail-Nguli <143516715+Abigail-Nguli@users.noreply.github.com>
2024-06-12 19:02:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function getshop(event) {
   let searchInput = document.querySelector("#search-input");
   let prompt = `Provide the best online shopping platform for: ${searchInput.value}`;
   let context = `Generate ONLY ONE online shopping platform that you think is the best. The results should be displayed in html format and include a <div class="site-name">title of an existing shopping platform</div>, 
-  then a brief explanation about the platform and its services and products(this should not exceed 5 lines).
+  then a brief explanation about the platform and its services and products.
   it should also include a link to the shopping platform with the following text: <div class="site-link">👉<a>Visit [title of the shopping platform] NOW(this should be active link to the shopping platform)</a>👈</div>. The explanation should be divided into separate lines by <br>. The last line should be: <div class="sign-off"> ❤️Happy Shopping❤️</div>`;
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
